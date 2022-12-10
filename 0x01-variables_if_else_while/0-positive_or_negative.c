2dbbe4dc2f84f5fa01bb0ff9dd98ed41ab61cae4
Abel Adisu <abeladisu73@gmail.com>
2022-12-09 18:13:48 -0800

    Positive negative and zero checking c programm

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <time.h>
 
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - main function to generate a random number
+ *
+ * Return: Always @ (Success)
+ *
+ */
 
 int main(void)
 {
@@ -13,11 +16,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
-	if(n > 0)
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if(n < 0)
+	else if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
