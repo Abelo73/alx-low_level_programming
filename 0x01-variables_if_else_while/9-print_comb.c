871f53c885fb0afec685d24784f779672868deb8
Abel Adisu <abeladisu73@gmail.com>
2022-12-10 01:01:40 -0800

    prints0 to 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - prints decimal numbers 0 to 9
+ *
+ */
+int main(void)
+{
+	int d = '0';
+	while (d >=0 )
+	{
+		putchar(d);
+		putchar(",");
+		putchar(" ");
+	}
+	return (0);
+}
