3a9efeaa21a528c913390f52c0876d81fc46dd03
Abel Adisu <abeladisu73@gmail.com>
2022-12-10 00:19:20 -0800

    prints alphabet reversly

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints small aphabets reversly
+ *
+ * Return: Always @ (Success)
+ *
+ */
+
+int main(void)
+{
+	int ch = 'z';
+	while (ch <=z )
+	{
+		putchar(ch, "\n");
+		ch++;
+	}
+	return (0);
+}
