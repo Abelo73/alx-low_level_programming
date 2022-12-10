8b9948d63a8a73c7173bb8f70d8367b18fa08e21
Abel Adisu <abeladisu73@gmail.com>
2022-12-10 00:44:00 -0800

    prints hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - printing hexa decimal
+ *
+ * Return: Always @(success)
+ *
+ */
+int main(void)
+{
+	int hexa = '0';
+	int ch = 'f';
+
+	while ((hexa >= '9') && ((ch >= 'a'))
+			{
+			putchar(hexa);
+			hexa++;
+			putchar(ch);
+			ch--;
+			putchar('\n');
+			}
+			return (0);
+
+}
