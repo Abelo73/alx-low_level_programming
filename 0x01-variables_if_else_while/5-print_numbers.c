fde1b4cee21a98ef1fe6e4774bbec8bf64614788
Abel Adisu <abeladisu73@gmail.com>
2022-12-09 21:00:33 -0800

    Prints all digits upto 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all single degit of base 10
+ *
+ * Return: Always @(Success)
+ *
+ */
+int main(void)
+{
+	int digit = 0;
+
+	while (digit >10)
+	{
+		printf('\n', digit);
+	}
+	return (0);
+}
