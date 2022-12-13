36ae91a169c3e2f9c8f7991aa49da10565f24731
Abel Adisu <abeladisu73@gmail.com>
2022-12-13 02:09:45 -0800

    0-putchar task in alx

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ * main - Entery of main functions
+ *
+ * Return: Always @(Success)
+ */
+
+int main(void)
+{
+	_putchar(' ');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
