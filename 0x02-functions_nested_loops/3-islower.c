cfc6de6d402413164436d22f45180832536d302a
Abel Adisu <abeladisu73@gmail.com>
2022-12-13 03:10:18 -0800

    Write a function that checks for lowercase character.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _islower - Retun 1 function that checks for lowercase character.
  * @c: char type letter for cheching character is lower or not
- * Retun: Always 0;
+ * Return: Always 0;
  */
 
 int _islower(int c)
