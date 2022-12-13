7e5802a17c30fc19c539a8159180698437a5bc75
Abel Adisu <abeladisu73@gmail.com>
2022-12-13 02:33:11 -0800

    1-alphabet in c programming that prints small letters

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints alphabets in small
+ *
+ * Return: Always 0.
+ *
+ */
+int print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(ch);
+		c++;
+	}
+	_putchar('\n');
+}
