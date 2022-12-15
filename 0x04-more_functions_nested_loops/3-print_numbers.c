8b75458bc7fbde4d99586da54870d27dcd007c23
Abel Adisu <abeladisu73@gmail.com>
2022-12-15 02:48:53 -0800

    Write a function that prints the numbers, from 0 to 9, followed by a new line.

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,19 @@
-#include "main.h"
-/**
-*print_numbers- a function that prints the numbers, from 0 to 9
-*
-*Return: print to stdout the alphabet, in lowercase, followed by a new line.
-**/
+/*
+ * File: 3-print_numbers.c
+ * Auth: Ephrem Abebe
+ */
 
+#include "main.h"
+
+/**
+ * print_numbers - Prints the numbers from 0-9.
+ */
 void print_numbers(void)
 {
-	int i;
+	int num;
 
-	for (i = 0; i <= 9; i++)
-		_putchar(i + '0');
+	for (num = 0; num <= 9; num++)
+		_putchar((num % 10) + '0');
+
 	_putchar('\n');
-}
+}}
