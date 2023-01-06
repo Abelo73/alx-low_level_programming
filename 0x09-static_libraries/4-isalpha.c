66e0d9e811c4658554ceb7a129ae3b80bd06a6cd
Abel Adisu <abeladisu73@gmail.com>
2023-01-06 13:22:01 -0800

    Create  the static library libmy.a containing all the function listed

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - functons that checks for alphabetic character
+ *
+ * @c: char type letter
+ * Return: 1 if lower case else prints 0
+ */
+int _isalpha(int c)
+{
+        if (c >= 65 && c <= 123)
+                return (1);
+        else
+                return (0);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of an integer
+ * @a: int type number
+ * Return: absolute value of @a
+ */
+int _abs(int a)
+{
+        if (a < 0)
+        {
+                return (a * -1);
+        }
+        else
+        {
+                return (a);
+        }
+}
