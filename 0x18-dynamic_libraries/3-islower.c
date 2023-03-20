30c770efb91d994216301addaa23bea325503252
Abel Adisu <abeladisu73@gmail.com>
2023-03-20 04:38:05 -0700

    task 2

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - Retun 1 function that checks for lowercase character.
+ * @c: char type letter for cheching character is lower or not
+ * Return: Always 0;
+ */
+
+int _islower(int c)
+{
+	if (c > 96 && c < 123)
+		return (1);
+	else
+		return (0);
+}
